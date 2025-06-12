edabf7c53ea0540c5127136735933a7db7d567b1
toyAE86 <150072766+toyAE86@users.noreply.github.com>
2025-06-12 13:39:52 -0400

    Inventory Drawer
    - Inventory Panel and drawer functionality
    
    -place holder slots for items

---
+++ Assets/Scripts/InventoryDrawer.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryDrawer : MonoBehaviour
+{
+    public RectTransform drawerPanel;
+    public Button toggleButton;
+    public float slideSpeed = 5f;
+
+    private Vector2 shownPosition;
+    private Vector2 hiddenPosition;
+    private bool isOpen = false;
+
+    void Start()
+    {
+        // Get current shown position
+        shownPosition = drawerPanel.anchoredPosition;
+        // Set hidden position (off screen to the left)
+        hiddenPosition = shownPosition + new Vector2(-drawerPanel.rect.width, 0);
+
+        // Start hidden
+        drawerPanel.anchoredPosition = hiddenPosition;
+
+        // Set up button
+        toggleButton.onClick.AddListener(ToggleDrawer);
+    }
+
+    void ToggleDrawer()
+    {
+        isOpen = !isOpen;
+    }
+
+    void Update()
+    {
+        Vector2 target = isOpen ? shownPosition : hiddenPosition;
+        drawerPanel.anchoredPosition = Vector2.Lerp(drawerPanel.anchoredPosition, target, Time.deltaTime * slideSpeed);
+    }
+}
