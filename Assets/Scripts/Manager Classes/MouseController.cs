0e3d40c9c6b9c07f6a039ee4946d33f1123359f0
EternalNyaki <124942024+EternalNyaki@users.noreply.github.com>
2025-07-08 12:29:49 -0400

    Fixed interacting with objects through UI

--- Assets/Scripts/Manager Classes/MouseController.cs
+++ Assets/Scripts/Manager Classes/MouseController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Apple.ReplayKit;
+using UnityEngine.EventSystems;
 
 public class MouseController : Manager<MouseController>
 {
@@ -30,7 +31,7 @@ public class MouseController : Manager<MouseController>
         RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(mousePos), Vector2.zero);
 
         // If the raycast hits something with the Interactable component, change the cursor to the hand
-        if ((hit.collider?.gameObject != null && hit.collider.gameObject.tag == "Interactable") || hit.collider?.GetComponent<Interactable>() != null)
+        if ((hit.collider?.gameObject != null && hit.collider.gameObject.tag == "Interactable") || hit.collider?.GetComponent<Interactable>() != null && !EventSystem.current.IsPointerOverGameObject())
         {
             Cursor.SetCursor(interactiveCursor, hotspot, CursorMode.Auto); // Change to interactive cursor
 
