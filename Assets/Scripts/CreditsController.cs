adf43ef3be7bc8e9a4d99d3303ef5b2ac55952b0
EternalNyaki <124942024+EternalNyaki@users.noreply.github.com>
2025-07-13 12:22:01 -0400

    Credits functionality

--- Assets/Scripts/CreditsController.cs
+++ Assets/Scripts/CreditsController.cs
@@ -3,8 +3,30 @@ using UnityEngine.SceneManagement;
 
 public class CreditsController : MonoBehaviour
 {
+    public AnimationCurve movementCurve;
+    public float scrollDuration;
+
+    public Camera creditsCamera;
+
+    public Vector3 startPosition;
+    public Vector3 endPosition;
+
+    private float _t;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        creditsCamera.transform.position = Vector3.Lerp(startPosition, endPosition, movementCurve.Evaluate(_t));
+
+        _t += Time.deltaTime / scrollDuration;
+    }
+
     public void OnBackToMenuClicked()
     {
-        SceneManager.LoadScene("Main Menu"); 
+        SceneManager.LoadScene("Main Menu");
     }
 }
